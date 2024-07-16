9db927cba374899402c9b8853bf4e79f887c1ec1
Alex Wellnitz <alex.wellnitz@materna.group>
2024-07-16 13:01:30 +0200

    feat: Initialize Terraform, Ansible and HCloud configurations for infrastructure management

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 variable "hcloud_token" {
   sensitive = true
+  type = string
 }
+
+variable "vm_admin_username" {
+  type = string
+  default = "root"
+}
+
 
 provider "hcloud" {
   token = var.hcloud_token
@@ -14,4 +21,27 @@ resource "hcloud_server" "vault" {
         ipv4_enabled = true
         ipv6_enabled = true
     }
+    ssh_keys = ["awellnit@MB-FVFHN41RQ05N", "alex@thinkpad"]
+}
+
+resource "ansible_host" "inventory" {
+  name   = hcloud_server.vault.ipv4_address
+  groups = ["vault"]
+  variables = {
+    ansible_user                 = var.vm_admin_username
+    # ansible_ssh_private_key_file = ".ssh/nomad_key"
+    ansible_python_interpreter   = "/usr/bin/python3"
+  }
+  depends_on = [hcloud_server.vault]
+}
+
+resource "ansible_playbook" "playbook" {
+  playbook   = "playbook.yml"
+  name       = hcloud_server.vault.ipv4_address
+  replayable = true
+
+  extra_vars = {
+    var_a = "Some variable"
+    var_b = "Another variable"
+  }
 }
--- versions.tf
+++ versions.tf
@@ -1,8 +1,13 @@
 terraform {
   required_providers {
     hcloud = {
-      source = "hetznercloud/hcloud"
+      source  = "hetznercloud/hcloud"
       version = "~> 1.45"
+    }
+
+    ansible = {
+      version = "~> 1.3.0"
+      source  = "ansible/ansible"
     }
   }
 }
